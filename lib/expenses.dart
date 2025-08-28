3f0f7b7d4a76e2ef7427d60641477580c6e629e5
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-08-28 16:40:36 +0530

    feat: add list view for expenses list

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,10 +11,28 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Flutter Course",
+      amount: 499,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "Cinema",
+      amount: 699,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [Text("Chart"), Text("Expense List")]),
+      body: Column(children: [Text("Chart"),
+       Text("Expense List")
+       
+       ]),
     );
   }
 }
