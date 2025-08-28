99d94f3717b7c964e848eb14dd712bf6e00ff0f7
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-08-28 17:21:16 +0530

    feat: add card view for the each expense item in the expenses list

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -29,10 +30,12 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [Text("Chart"),
-       Text("Expense List")
-       
-       ]),
+      body: Column(
+        children: [
+          Text("Chart"),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
     );
   }
 }
