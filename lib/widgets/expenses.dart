760772787fc7a2f668537bbbea03f91f62c3aef3
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-08-29 11:23:18 +0530

    feat: add functionality for adding the expense

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -27,12 +27,21 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => Text("Model bottom sheet"),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Expense Tracker"),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [
+          IconButton(onPressed: _openAddExpenseOverlay, icon: Icon(Icons.add)),
+        ],
       ),
       body: Column(
         children: [
