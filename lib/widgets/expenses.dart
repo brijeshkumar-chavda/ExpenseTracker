6721fe17c72a2cc6c48b276229af9fafc6ba8486
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-08-29 11:06:09 +0530

    feat: add app bar

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -30,6 +30,10 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Expense Tracker"),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+      ),
       body: Column(
         children: [
           Text("Chart"),
