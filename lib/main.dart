f939e990c3a2b1db1d07e1a014a9ca7bcfcc81ea
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-08-29 14:28:12 +0530

    feat: add save expense button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:expense_tracker/widgets/expenses.dart';
+import 'package:expense_tracker/widgets/expenses_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Expenses()));
+  runApp(MaterialApp(home: ExpensesScreen()));
 }
--- lib/widgets/expenses_screen.dart
+++ lib/widgets/expenses_screen.dart
@@ -1,17 +1,18 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
-class Expenses extends StatefulWidget {
-  const Expenses({super.key});
+class ExpensesScreen extends StatefulWidget {
+  const ExpensesScreen({super.key});
 
   @override
   State<StatefulWidget> createState() {
-    return _ExpensesState();
+    return _ExpensesScreenState();
   }
 }
 
-class _ExpensesState extends State<Expenses> {
+class _ExpensesScreenState extends State<ExpensesScreen> {
   final List<Expense> _registeredExpenses = [
     Expense(
       title: "Flutter Course",
@@ -28,10 +29,7 @@ class _ExpensesState extends State<Expenses> {
   ];
 
   void _openAddExpenseOverlay() {
-    showModalBottomSheet(
-      context: context,
-      builder: (ctx) => Text("Model bottom sheet"),
-    );
+    showModalBottomSheet(context: context, builder: (ctx) => NewExpense());
   }
 
   @override
