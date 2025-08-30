d7b0938ecc179ada638bfd6ebcc32e59f5a579a7
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-08-30 19:11:04 +0530

    feat: add alert dialog for invalid input

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -35,6 +35,34 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final isAmountInvalid = enteredAmount == null || enteredAmount <= 0;
+
+    if (_titleController.text.trim().isEmpty ||
+        isAmountInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Text("Invalid Input"),
+          content: Text(
+            "Please make sure valid title, amount, date and category was entered.",
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: Text("Okay"),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -116,10 +144,7 @@ class _NewExpenseState extends State<NewExpense> {
               ),
               SizedBox(width: 12),
               ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                  print(_amountController.text);
-                },
+                onPressed: _submitExpenseData,
                 child: Text("Save Expense"),
               ),
             ],
