d285efd7eeb50fb22fa683373276d81d0f61cab8
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-09-01 15:15:19 +0530

    feat: add undo snackbar

--- lib/widgets/expenses_screen.dart
+++ lib/widgets/expenses_screen.dart
@@ -43,9 +43,26 @@ class _ExpensesScreenState extends State<ExpensesScreen> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: Duration(seconds: 3),
+        content: Text("Expense deleted"),
+        action: SnackBarAction(
+          label: "Undo",
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
