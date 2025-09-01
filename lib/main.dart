756c158fe061893f3a38741aabcdbaa5cc7110af
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-09-01 16:31:18 +0530

    feat: add dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,54 @@
 import 'package:expense_tracker/widgets/expenses_screen.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 96, 59, 181),
+);
+
+var kDarkColorTheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+);
 void main() {
-  runApp(MaterialApp(home: ExpensesScreen()));
+  runApp(
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorTheme,
+        cardTheme: CardThemeData().copyWith(
+          color: kDarkColorTheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorTheme.primaryContainer,
+            foregroundColor: kDarkColorTheme.onPrimaryContainer,
+          ),
+        ),
+      ),
+      home: ExpensesScreen(),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          // backgroundColor: kColorScheme.onPrimaryContainer,
+          // foregroundColor: kColorScheme.onPrimaryContainer,
+        ),
+        cardTheme: CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      themeMode: ThemeMode.system,
+    ),
+  );
 }
